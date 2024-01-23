bdab3d091bdbb99c05a01bda92d13de95d06ff87
saikrishnasaski <saikrishnachitti19@gmail.com>
2024-01-23 10:36:00 +0530

    basic setup

---
+++ library-events-producer/src/test/java/com/csk/services/library/events/producer/LibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csk.services.library.events.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
